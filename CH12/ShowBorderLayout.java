6ec31e7d833280871f97ad42facb3f9d79f12988
EdwardC2J <yywzc2j@163.com>
2019-02-19 18:32:40 +0800

    Add files via upload

---
+++ CH12/ShowBorderLayout.java
@@ -0,0 +1,21 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class ShowBorderLayout extends JFrame{
+	public ShowBorderLayout() {
+		setLayout(new BorderLayout(5,10));
+		add(new JButton("East"),BorderLayout.EAST);
+		add(new JButton("South"),BorderLayout.SOUTH);
+		add(new JButton("West"),BorderLayout.WEST);
+		add(new JButton("North"),BorderLayout.NORTH);
+		add(new JButton("Center"),BorderLayout.CENTER);
+	}
+	public static void main(String[] args) {
+		ShowBorderLayout frame=new ShowBorderLayout();
+		frame.setSize(300, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
